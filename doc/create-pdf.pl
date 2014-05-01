eb2e462e129af812ddc61e3746570d5f00b5b6b8
Aaron Petkau <aaron.petkau@phac-aspc.gc.ca>
2014-05-01 15:07:23 -0500

    Added title to documents

--- doc/create-pdf.pl
+++ doc/create-pdf.pl
@@ -15,16 +15,16 @@ my $lab_dir_orig = "$script_dir/../labs";
 my $lab_dir = "$script_dir/labs";
 
 my %file_properties = (
-	'core-snp/README.md' => 'Day7PetkauCoreSNPLab.pdf',
-	'core-snp/Answers.md' => 'Day7PetkauCoreSNPAnswers.pdf',
-	'ffp-phylogeny/README.md' => 'Day7PetkauFFPLab.pdf',
-	'ffp-phylogeny/Answers.md' => 'Day7PetkauFFPAnswers.pdf',
-	'gview-server/README.md' => 'Day6PetkauGViewServerLab.pdf',
-	'gview-server/Answers.md' => 'Day6PetkauGViewServerAnswers.pdf',
-	'orthomcl/README.md' => 'Day6PetkauOrthoMCLLab.pdf',
-	'orthomcl/Answers.md' => 'Day6PetkauOrthoMCLAnswers.pdf',
-	'mst/README.md' => 'Day6PetkauMSTLab.pdf',
-	'mst/Answers.md' => 'Day6PetkauMSTAnswers.pdf'
+	'core-snp/README.md' => {'file'=>'Day7PetkauCoreSNPLab.pdf', 'title'=>'Core SNP Phylogenies'},
+	'core-snp/Answers.md' => {'file'=>'Day7PetkauCoreSNPAnswers.pdf', 'title'=>'Core SNP Phylogenies: Answers'},
+	'ffp-phylogeny/README.md' => {'file'=>'Day7PetkauFFPLab.pdf', 'title'=>'Feature Frequency Profile Phylogenies'},
+	'ffp-phylogeny/Answers.md' => {'file'=>'Day7PetkauFFPAnswers.pdf', 'title'=>'Feature Frequency Profile Phylogenies: Answers'},
+	'gview-server/README.md' => {'file'=>'Day6PetkauGViewServerLab.pdf', 'title'=>'Working with GView Server'},
+	'gview-server/Answers.md' => {'file'=>'Day6PetkauGViewServerAnswers.pdf', 'title'=>'Working with GView Server: Answers'},
+	'orthomcl/README.md' => {'file'=>'Day6PetkauOrthoMCLLab.pdf', 'title'=>'Ortholog detection with OrthoMCL'},
+	'orthomcl/Answers.md' => {'file'=>'Day6PetkauOrthoMCLAnswers.pdf', 'title'=>'Ortholog detection with OrthoMCL: Answers'},
+	'mst/README.md' => {'file'=>'Day6PetkauMSTLab.pdf', 'title'=>'Minimum Spanning Trees with PHYLOViZ'},
+	'mst/Answers.md' => {'file'=>'Day6PetkauMSTAnswers.pdf', 'title'=>'Minimum Spanning Trees with PHYLOViZ: Answers'}
 );
 
 my $author = "Aaron Petkau";
@@ -50,13 +50,14 @@ system($command);
 
 for my $file (keys %file_properties)
 {
-	my $name = $file_properties{$file};
+	my $name = $file_properties{$file}{'file'};
+	my $title = $file_properties{$file}{'title'};
 	my $file_path = "$lab_dir/$file";
 	my $file_path_dir = dirname("$lab_dir/$file");
 
 	chdir $file_path_dir;
 
-	my $command = "pandoc --template=$template --toc --highlight-style $highlight -Vauthor=\"$author\" -Vgeometry:$geometry -f markdown+pipe_tables -o $doc_dir/$name $file_path";
+	my $command = "pandoc --template=$template --toc --highlight-style $highlight -Vtitle=\"$title\" -Vauthor=\"$author\" -Vgeometry:$geometry -f markdown+pipe_tables -o $doc_dir/$name $file_path";
 	print "$command\n";
 	system($command) == 0 || die "Could not run \"$command\"";
 
