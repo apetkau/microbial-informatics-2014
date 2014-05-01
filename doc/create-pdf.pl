79519e908d144dc18e4392cfad4e020268f52667
Aaron Petkau <aaron.petkau@phac-aspc.gc.ca>
2014-05-01 17:11:37 -0500

    Just resize images instead

--- doc/create-pdf.pl
+++ doc/create-pdf.pl
@@ -32,15 +32,13 @@ my $geometry = "margin=1in";
 my $highlight = "monochrome";
 my $template = "$script_dir/default.latex";
 my $command;
-my $density_scale = 0.5;
 
 # prepare directory with labs
 system("rm -r $lab_dir") if (-e $lab_dir);
 system("cp -r $lab_dir_orig $lab_dir");
 
 # prepare images by converting the dpi 
-#$command = "find $lab_dir -iname '*.jpg' | xargs -I {} convert {} -resample '$dpi_to_convert' {}";
-$command = "for i in `find labs/ -iname '*.jpg'`; do b=`identify -format \"\%x\" \$i`; units=`echo \$b | cut -d ' ' -f 2`; value=`echo \$b | cut -d ' ' -f 1`; newvalue=`echo $density_scale*\$value | bc`; convert \$i -units \$units -density \$value -resample \$newvalue \$i; done";
+$command = "find $lab_dir -iname '*.jpg' | xargs -I {} convert {} -resize 65% {}";
 print "$command\n";
 system($command);
 
